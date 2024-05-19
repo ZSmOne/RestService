faeb87b85a7a199cc35914888d4081ac0b07c39d
ZSmOne <Vlad9539.sorokin@gmail.com>
2024-05-19 17:20:36 +0300

    Test jenkins hook

--- src/main/java/org/rest/servlet/CityServlet.java
+++ src/main/java/org/rest/servlet/CityServlet.java
@@ -55,10 +55,10 @@ public class CityServlet extends HttpServlet {
         try {
             String[] pathPart = req.getPathInfo().split("/");
             if ("all".equals(pathPart[1])) {
-                List<CityOutGoingDto> cityDtoList = cityDtoMapper.map(cityService.findAll());
+                //List<CityOutGoingDto> cityDtoList = cityDtoMapper.map(cityService.findAll());
                 resp.setStatus(HttpServletResponse.SC_OK);
-                response = objectMapper.writeValueAsString(cityDtoList);
-                //response = "{\"gete\":\"test city\"}";
+                //response = objectMapper.writeValueAsString(cityDtoList);
+                response = "{\"gete\":\"test city\"}";
             } else {
                 Long cityId = Long.parseLong(pathPart[1]);
                 CityOutGoingDto cityDto = cityDtoMapper.map(cityService.findById(cityId));
